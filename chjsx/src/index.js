1917b9483ae0f2a23a111d547b00b9b5aee1d88b
azazawejden <azazawejden@gmail.com>
2023-03-23 11:35:49 +0100

    first commit

--- chjsx/src/index.js
+++ chjsx/src/index.js
@@ -1,18 +1,16 @@
+
 import React from 'react';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-
-function ProfilePhoto() {
-  return (
-      <img
-          src="https://gomycodelearndev.blob.core.windows.net/profiles/bdd32436-2afe-431a-a990-66cdeb74bdb9-133195610347670294.jpg"
-          alt="My Profile Photo"
-      />
-  );
-}
-
-export default ProfilePhoto;
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
 
 // If you want to start measuring performance in your app, pass a function
